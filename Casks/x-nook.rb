405529726a753b0ccbaaf77b9f582ac4de913a54
Meteorkid <2838801110@qq.com>
2026-07-15 18:02:40 +0800

    release: update cask for v1.3.13

--- Casks/x-nook.rb
+++ Casks/x-nook.rb
@@ -1,8 +1,8 @@
 # 注意：版本号应与 VERSION 文件保持一致
 # 更新步骤：1. 修改 VERSION 文件 2. 构建 DMG 3. 更新此文件的 version 和 sha256
 cask "x-nook" do
-  version "1.3.12"
-  sha256 "e12041d436e19f5f6bfc29933c3374597abe60e945445c20b5aee8c94af188fb"
+  version "1.3.13"
+  sha256 "328d74623584258cc91110e184888a8df5836d50ed690b5dc632982eb86d3f42"
 
   url "https://github.com/Meteorkid/XNook/releases/download/v#{version}/XNook-#{version}.dmg",
       verified: "github.com/Meteorkid/XNook/"
